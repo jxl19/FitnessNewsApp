78be3cc47cba4de55165d94417594f5ae4fb67fe
Jun Lei <jlei91@gmail.com>
2021-04-17 23:08:04 -0700

    upload to s3, download pdf

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF }  from 'jspdf';
+import html2canvas from 'html2canvas';
 import { Newsletters } from '../models/newsletters';
 import { NewsletterServiceService } from '../services/newsletter-service.service';
+import { UploadServiceService } from '../services/upload-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-newsletter',
@@ -8,8 +12,10 @@ import { NewsletterServiceService } from '../services/newsletter-service.service
   styleUrls: ['./newsletter.component.css'],
 })
 export class NewsletterComponent implements OnInit {
-  constructor(private newsletterService: NewsletterServiceService) {}
+  
+  constructor(private newsletterService: NewsletterServiceService, private uploadService : UploadServiceService, private http: HttpClient) {}
 
+  @ViewChild('newsletter') htmlData:ElementRef | any;
   newsletter: Newsletters | any;
   mostRecentId: number = 0;
   prevId: number = 0;
@@ -35,5 +41,45 @@ export class NewsletterComponent implements OnInit {
           console.log(this.newsletter);
         });
     }
+  }
+  
+  downloadPDF() {
+    console.log("downloading..");
+    const doc = new jsPDF();
+    let data:any = document.getElementById('newsletter');
+    html2canvas(data).then((canvas => {
+      let width = 250;
+      let fileHeight =  250;
+      const fileUri = canvas.toDataURL('image/png', 0.1 );
+      
+      let pdf = new jsPDF('p', 'mm', [250, 250]);
+      let pos = 0;
+      pdf.addImage(fileUri, 'png', 0, pos, width, fileHeight, undefined, 'FAST');
+      pdf.save('demo.pdf');
+    }))
+  }
+
+  uploadPDF() {
+    const doc = new jsPDF();
+    let data:any = document.getElementById('newsletter');
+    html2canvas(data).then(((canvas: { toDataURL: (arg0: string, arg1: number) => any; }) => {
+      let width = 250;
+      let fileHeight =  250;
+      const fileUri = canvas.toDataURL('image/png', 0.1);
+      let pdf = new jsPDF('p', 'mm', [250, 250]);
+      let pos = 0;
+      pdf.addImage(fileUri, 'png', 0, pos, width, fileHeight, undefined, 'FAST');
+      const formData = new FormData();
+      var out = pdf.output('blob');
+      formData.append("uploadFile", out, 'somename.pdf');
+      // this.uploadService.uploadPDF(formData).subscribe(data => {
+      //   console.log(data)
+      // });
+      console.log('fornd', formData);
+      this.http.post('http://localhost:8080/fileupload/file/', formData).subscribe(res => {
+        console.log(res);
+      })
+      console.log('done');
+    }))
   }
 }
---
+++ src/app/services/upload-service.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadServiceService {
+  baseUrl = 'http://localhost:8080/fileupload/file/'
+  constructor(private http: HttpClient) { }
+  uploadPDF(pdf: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl, pdf)
+    .pipe(
+      retry(1),
+      catchError(this.errorHandler)
+    )
+  }
+  
+  errorHandler(error:any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
