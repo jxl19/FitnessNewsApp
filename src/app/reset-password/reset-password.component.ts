4198557f808ec12882d6dbc4bd84cdde043d5a94
larios01 <larios@tamu.edu>
2021-04-19 20:18:12 -0500

    This redirects the user to the login page after they reset the password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -6,55 +6,39 @@ import { ResetServiceService } from '../services/reset-service.service';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css'],
+  styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-  password: string = '';
-  token: any;
-  status: string = '';
-  response: string = '';
-  confirmPassword: string= '';
 
-  constructor(
-    private resetservice: ResetServiceService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  
+  password:string = "";
+  token:any;
+  status:string = "";
+  response:string = "";
+
+  constructor(private resetservice:ResetServiceService, private router:Router, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    document.body.classList.add('landing');
-  }
-
-  ngOnDestroy(): void {
-    document.body.classList.remove('landing');
-  }
-
-  handleConfirmPassword(confirmPassword: string) {
-    this.confirmPassword = confirmPassword;
-  }
-
-  handlePassword(password: string) {
+}
+  handlePassword(password:string){
     this.password = password;
-  }
-
-  handleSubmit(e: any) {
+}
+handleSubmit(e:any) {
     e.preventDefault();
-    if (this.password === this.confirmPassword) {
-      const token = this.route.snapshot.queryParams;
-      let passwordtoken = { token: token.token, password: this.password };
-      this.resetservice
-        .postPasswordByToken(JSON.stringify(passwordtoken))
-        .subscribe((data) => {
-          console.log(data);
-          this.status = data.status;
-          this.response = data.response;
-        });
-    } else {
-      this.response = "Password does not match";
-    }
+    const token = this.route.snapshot.queryParams;
+    let passwordtoken= {"token": token.token, "password": this.password};
+    this.resetservice.postPasswordByToken(JSON.stringify(passwordtoken)).subscribe(data => {
+      console.log(data)
+      this.status = data.status
+      this.response = data.response
+      this.router.navigate(['/login']);
+    })
+    
   }
 
   goToLanding() {
     this.router.navigate(['/']);
   }
+
+
 }
